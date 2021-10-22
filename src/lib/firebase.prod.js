26ef0cebd93552412bf953ab956085c4e9f1ecb9
csaroya20 <88789623+csaroya20@users.noreply.github.com>
2021-10-22 21:38:21 +0530

    First commit

---
+++ src/lib/firebase.prod.js
@@ -0,0 +1,17 @@
+import Firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+  apiKey: 'AIzaSyCev-C6AKuyxD8oXcGwuAyyGmWpo7INl3U',
+  authDomain: 'netflix-test01.firebaseapp.com',
+  databaseURL: 'https://netflix-test01.firebaseio.com',
+  projectId: 'netflix-test01',
+  storageBucket: 'netflix-test01.appspot.com',
+  messagingSenderId: '541152959854',
+  appId: '1:541152959854:web:38145f65984eef0cd3757f',
+};
+
+const firebase = Firebase.initializeApp(config);
+
+export { firebase };
