a2d6a0c53c50c7783dbb91196a1f7905ea27c9db
csaroya20 <88789623+csaroya20@users.noreply.github.com>
2021-10-24 14:08:26 +0530

    Updates

---
+++ precache-manifest.b163d82bfd57f1bca759fd33a252cff8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92f62543b78b1c50d81b645dcb003a2e",
+    "url": "/csaroya20/netflixclone/index.html"
+  },
+  {
+    "revision": "61ed988ebfb9f7bb5eef",
+    "url": "/csaroya20/netflixclone/static/css/main.ab7a3f0f.chunk.css"
+  },
+  {
+    "revision": "e5fe4ed9db4389af5a87",
+    "url": "/csaroya20/netflixclone/static/js/2.0aaf957a.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/csaroya20/netflixclone/static/js/2.0aaf957a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61ed988ebfb9f7bb5eef",
+    "url": "/csaroya20/netflixclone/static/js/main.70c25353.chunk.js"
+  },
+  {
+    "revision": "9f7ea15dd8f314b9037c",
+    "url": "/csaroya20/netflixclone/static/js/runtime-main.623add05.js"
+  }
+]);
